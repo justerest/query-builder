e426fd8d85adf808f64af9c6195d5630aad27d00
justerest <justerest@gmailcom>
2020-03-26 00:11:32 +0500

    refactor: extract base command builder

---
+++ src/core/CommandBuilder.ts
@@ -0,0 +1,16 @@
+import { Command } from './commands/Command';
+import { Field } from './Field';
+import { Querier } from './Querier';
+
+export abstract class CommandBuilder<TCommand extends Command = Command> {
+  getAvailableCommands(fields: Field[], querier?: Querier): TCommand[] {
+    const querierCommands = querier?.getCommands() ?? [];
+    const querierCommandIdSet = new Set(querierCommands.map((command) => command.id));
+    return fields
+      .flatMap((field) => this.createCommands(field, querier))
+      .filter((command) => !querierCommandIdSet.has(command.id))
+      .filter((command) => command.compatible(querierCommands));
+  }
+
+  protected abstract createCommands(field: Field, querier?: Querier): TCommand[];
+}
--- src/core/GroupByCommandBuilder.ts
+++ src/core/GroupByCommandBuilder.ts
@@ -1,14 +1,9 @@
+import { CommandBuilder } from './CommandBuilder';
 import { GroupByCommand } from './commands/GroupByCommand';
 import { Field } from './Field';
-import { Querier } from './Querier';
 
-export class GroupByCommandBuilder {
-  getAvailableCommands(fields: Field[], querier?: Querier): GroupByCommand[] {
-    const querierCommands = querier?.getCommands() ?? [];
-    const querierCommandIdSet = new Set(querierCommands.map((command) => command.id));
-    return fields
-      .map((field) => new GroupByCommand(field))
-      .filter((command) => !querierCommandIdSet.has(command.id))
-      .filter((command) => command.compatible(querierCommands));
+export class GroupByCommandBuilder extends CommandBuilder<GroupByCommand> {
+  protected createCommands(field: Field): GroupByCommand[] {
+    return [new GroupByCommand(field)];
   }
 }
