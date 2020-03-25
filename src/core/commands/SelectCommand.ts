ad5fec03925409b7a9c28051c56f117a1c2f5c51
justerest <justerest@gmailcom>
2020-03-26 01:00:28 +0500

    fix: imcompatible SelectCommand with AggregateCommand

--- src/core/commands/SelectCommand.ts
+++ src/core/commands/SelectCommand.ts
@@ -1,5 +1,6 @@
 import { Command, CommandType } from '../Command';
 import { Field } from '../Field';
+import { AggregateCommand } from './AggregateCommand';
 import { GroupByCommand } from './groupBy/GroupByCommand';
 
 export class SelectCommand extends Command {
@@ -9,6 +10,13 @@ export class SelectCommand extends Command {
 
   constructor(field: Field) {
     super(CommandType.Select, field);
+  }
+
+  // TODO: уточнить как должно работать
+  // ?? может рекурсивно проверять relativeCommands?
+  // Добавил, потому что нельзя сочетать AggregateCommand с GroupBy
+  compatible(commands: Command[]): boolean {
+    return !commands.filter(this.isCommandWithSameField).some(AggregateCommand.isAggregateCommand);
   }
 
   relativeCommands(): Command[] {
