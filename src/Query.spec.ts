f6df9fec71d8749734f2beea0c024175f8179e30
justerest <justerest@gmailcom>
2020-03-25 12:29:51 +0500

    feat: init query/source methods

--- src/Query.spec.ts
+++ src/Query.spec.ts
@@ -1,4 +1,5 @@
 import { Query } from './Query';
+import { RequestSource } from './RequestSource';
 
 describe('Query', () => {
   let query: Query;
@@ -7,5 +8,15 @@ describe('Query', () => {
 
   it('should be created', () => {
     expect(query).toBeInstanceOf(Query);
+  });
+
+  it('+getSource() should returns undefined on init', () => {
+    expect(query.getSource()).toBeUndefined();
+  });
+
+  it('+setSource() should set RequestSource', () => {
+    const source = {} as RequestSource;
+    query.setSource(source);
+    expect(query.getSource()).toBe(source);
   });
 });
--- src/Query.ts
+++ src/Query.ts
@@ -1,1 +1,13 @@
-export class Query {}
+import { RequestSource } from './RequestSource';
+
+export class Query {
+  private source?: RequestSource;
+
+  getSource(): RequestSource | undefined {
+    return this.source;
+  }
+
+  setSource(source: RequestSource): void {
+    this.source = source;
+  }
+}
