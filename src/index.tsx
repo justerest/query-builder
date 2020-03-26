71fc97d1abade0239937941669377d1187345c5d
justerest <justerest@gmailcom>
2020-03-26 12:37:08 +0500

    feat: connect order by commands to react

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import { AggregateCommand } from './core/commands/AggregateCommand';
 import { GroupByCommand } from './core/commands/groupBy/GroupByCommand';
 import { GroupByCommandBuilder } from './core/commands/groupBy/GroupByCommandBuilder';
+import { OrderByCommand } from './core/commands/orderBy/OrderByCommand';
+import { OrderByCommandBuilder } from './core/commands/orderBy/OrderByCommandBuilder';
 import { PresentationCommandBuilder } from './core/commands/PresentationCommandBuilder';
 import { SelectCommand } from './core/commands/SelectCommand';
 import { Field } from './core/Field';
@@ -11,6 +13,7 @@ import { Querier } from './core/Querier';
 
 const presentationCommandBuilder = new PresentationCommandBuilder();
 const groupByCommandBuilder = new GroupByCommandBuilder();
+const orderByCommandBuilder = new OrderByCommandBuilder();
 const fields = [new Field('prop1', 'string'), new Field('prop2', 'number')];
 const querier = new Querier();
 
@@ -80,6 +83,37 @@ const App: React.FC = () => {
             }}
           >
             {command.field.name}
+          </li>
+        ))}
+      </ul>
+      <h3>Applied Order By Fields</h3>
+      <ul>
+        {querier
+          .getCommands()
+          .filter(OrderByCommand.isOrderByCommand)
+          .map((command) => (
+            <li
+              key={command.id}
+              onClick={() => {
+                querier.removeCommand(command);
+                update({});
+              }}
+            >
+              ordered by {command.field.name} {command.direction}
+            </li>
+          ))}
+      </ul>
+      <h3>Available Order By Fields</h3>
+      <ul>
+        {orderByCommandBuilder.getAvailableCommands(fields, querier).map((command) => (
+          <li
+            key={command.id}
+            onClick={() => {
+              querier.addCommand(command);
+              update({});
+            }}
+          >
+            {command.field.name} {command.direction}
           </li>
         ))}
       </ul>
