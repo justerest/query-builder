699a3cff419a18a33f6430c3215b4d48632cc156
justerest <justerest@gmailcom>
2020-03-26 21:00:18 +0500

    refactor: command builder filter

--- src/core/CommandBuilder.ts
+++ src/core/CommandBuilder.ts
@@ -2,13 +2,11 @@ import { Command } from './Command';
 import { Field } from './Field';
 import { Querier } from './Querier';
 
-export abstract class CommandBuilder<TCommand extends Command = Command> {
+export abstract class CommandBuilder<TCommand extends Command> {
   getAvailableCommands(fields: Field[], querier?: Querier): TCommand[] {
-    const querierCommands = querier?.getCommands() ?? [];
-    const querierCommandIdSet = new Set(querierCommands.map((command) => command.id));
     return fields
       .flatMap((field) => this.createCommands(field, querier))
-      .filter((command) => !querierCommandIdSet.has(command.id))
+      .filter((command) => !querier?.hasCommand(command))
       .filter((command) => !querier || command.compatible(querier));
   }
 
